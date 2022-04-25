a60184d6174094b4e218db1eecf05b5edfb6d0ef
ITstart <itstart@itstep.loc>
2022-04-25 17:44:12 +0600

    first commit

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <h1>Products</h1>
+    <div>
+    <div>
+        <h1>Products</h1>
+      </div>
+      <div>
+        <input type="search" id="title_search" placeholder="search..." />
+        <button @click="titleSearch">search</button>
+      </div>
+      </div>
     <div v-for="c in categories" :key="c.id" @click="pickCategory(c.id)">
       <div class="category_nav">{{ c.title }}</div>
     </div>
@@ -12,10 +20,16 @@
       <option value="price:desc">priceDesc</option>
       <option value="updatedAt:desc">newest</option>
     </select>
+
+    <p>{{ search }}</p>
     <div v-for="p in products" :key="p.id" class="card" @click="move(p.id)">
       <p>{{ p.title }}</p>
       <p>{{ p.price }}</p>
+      
     </div>
+  <div class="in-block">
+    <div v-for="page in pages" :key="page" @click="moveToPage(page)" class="button">{{ page }}</div>
+  </div>
   </div>
 </template>
 
@@ -29,32 +43,36 @@ export default {
       products: [],
       priceSort: "updatedAt:desc",
       categories: [],
+      pages: 1,
+      search: null,
+      selPage: 1
     };
   },
   async mounted() {
     const c_query = qs.stringify(
       {
         sort: ["title"],
-        
       },
       { encodeValuesOnly: true }
     );
-    const { data: c } = await axios.get("http://localhost:1337/api/categories?" + c_query);
+    const { data: c } = await axios.get(
+      "http://localhost:1337/api/categories?" + c_query
+    );
     this.categories = c.data.map((product) => {
       return {
         id: product.id,
         ...product.attributes,
       };
     });
-    const id = this.categories[0].id
-    this.$router.push("?categoryId=" + id)
-    
+
+    const id = this.categories[0].id;
+    this.$router.push("?categoryId=" + id);
     const p_query = qs.stringify(
       {
         sort: ["updatedAt:desc"],
         filters: {
           category: {
-            id:{
+            id: {
               $eq: Number(id),
             },
           },
@@ -77,20 +95,21 @@ export default {
       this.$router.push("/products/" + id);
     },
     async onPriceSortChange() {
-      const id = this.$route.query.categoryId;
+      console.log(this.$route.query.categoryId);
       const query = qs.stringify(
         {
           sort: [this.priceSort],
           filters: {
-          category: {
-            id:{
-              $eq: Number(id),
+            category: {
+              id: {
+                $eq: Number(this.$route.query.categoryId),
+              },
             },
           },
         },
-        },
         { encodeValuesOnly: true }
       );
+
       const { data } = await axios.get(
         "http://localhost:1337/api/products?" + query
       );
@@ -102,7 +121,6 @@ export default {
       });
     },
     async priceFilter() {
-      const id = this.$route.query.categoryId;
       const query = qs.stringify(
         {
           filters: {
@@ -116,13 +134,13 @@ export default {
                 price: {
                   $lte: Number(document.getElementById("price_lte").value),
                 },
+                category: {
+                  id: {
+                    $eq: Number(this.$route.query.categoryId),
+                  },
+                },
               },
             ],
-            category: {
-            id:{
-              $eq: Number(id),
-            }
-          },
           },
         },
         { encodeValuesOnly: true }
@@ -139,30 +157,94 @@ export default {
     },
     async pickCategory(id) {
       const p_query = qs.stringify(
-      {
-        populate: "category",
-        sort: ["updatedAt:desc"],
-        filters: {
-          category: {
-            id:{
-              $eq: Number(id),
-            }
+        {
+          populate: "category",
+          sort: ["updatedAt:desc"],
+          filters: {
+            category: {
+              id: {
+                $eq: Number(id),
+              },
+            },
+          },
+          pagination: {
+            page: this.selPage,
+            pageSize: 3,
           },
         },
-      },
-      { encodeValuesOnly: true }
-    );
-    const { data: p } = await axios.get(
-      "http://localhost:1337/api/products?" + p_query
-    );
-    this.products = p.data.map((product) => {
+        { encodeValuesOnly: true }
+      );
+      const { data: p } = await axios.get(
+        "http://localhost:1337/api/products?" + p_query
+      );
+      this.products = p.data.map((product) => {
         return {
           id: product.id,
           ...product.attributes,
         };
       });
-      this.$router.push("?categoryId=" + id)
-    }
+      console.log (p_query)
+      this.pages = p.meta.pagination.pageCount
+      this.$router.push("/products?categoryId=" + id);
+    },
+    async moveToPage(page) {
+      this.selPage = page
+      console.log(this.selPage)
+      const p_query = qs.stringify(
+        {
+          populate: "category",
+          sort: ["updatedAt:desc"],
+          filters: {
+            category: {
+              id: {
+                $eq: Number(this.$route.query.categoryId),
+              },
+            },
+          },
+          pagination: {
+            page: this.selPage,
+            pageSize: 5,
+          },
+        },
+        { encodeValuesOnly: true }
+      );
+      const { data: p } = await axios.get(
+        "http://localhost:1337/api/products?" + p_query
+      );
+      this.products = p.data.map((product) => {
+        return {
+          id: product.id,
+          ...product.attributes,
+        };
+      });
+      },
+    async titleSearch() {
+      const id = this.$route.query.categoryId;
+      const query = qs.stringify(
+        {
+          filters: {
+                title: {
+                  $contains: document.getElementById("title_search").value,
+                },
+              },
+            category: {
+            id:{
+              $eq: Number(id),
+            }
+          },
+          },
+        { encodeValuesOnly: true }
+      );
+      const { data } = await axios.get(
+        "http://localhost:1337/api/products?" + query
+      );
+      this.products = data.data.map((product) => {
+        return {
+          id: product.id,
+          ...product.attributes,
+        };
+      });
+    },
   },
 };
 </script>
@@ -174,6 +256,7 @@ export default {
   margin-bottom: 20px;
   box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
 }
+
 .card:hover {
   cursor: pointer;
   box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
@@ -185,7 +268,28 @@ export default {
   margin-bottom: 20px;
   border: 3px solid black;
 }
+
 .category_nav:hover {
   cursor: pointer;
+}
+
+.block{
+  display: flex;
+  align-items: center;
+    justify-content: space-between;
+}
+
+.in-block{
+  display: flex;
+}
+
+.button{
+  padding: 10px;
+    background-color: #9d8af9;
+    width: 20px;
+    text-align: center;
+    margin: 2px;
+    color: white;
+
 }
 </style>
